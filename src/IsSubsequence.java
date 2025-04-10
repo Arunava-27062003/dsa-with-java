b2e006c212e4441a7ae0fb9523e08af646209ba5
Arunava-27062003 <arunava.kundu@iemlabs.com>
2025-04-10 14:16:01 +0530

    sub sequence not complete

---
+++ src/IsSubsequence.java
@@ -0,0 +1,13 @@
+public class IsSubsequence {
+    public static void main(String[] args) {
+        String s = "abc";
+        String t = "ahbgdc";
+        boolean res = findSubSequence(s, t);
+        System.out.println(res);
+    }
+    public static boolean findSubSequence(String s, String t){
+
+        
+        return true;
+    }
+}
